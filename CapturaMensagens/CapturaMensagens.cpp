3e590774455e70d48af209c040b4503ceac3245f
Thiagorss54 <thiagorss54@gmail.com>
2022-01-07 18:33:52 -0300

    [Thiago] Add função SepararString

--- CapturaMensagens/CapturaMensagens.cpp
+++ CapturaMensagens/CapturaMensagens.cpp
@@ -1,12 +1,6 @@
 // CapturaMensagens.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
 //
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
 
 // Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
 // Depurar programa: F5 ou menu Depurar > Iniciar Depuração
---
+++ LeituraSupervisorio/LeituraSupervisorio.cpp
@@ -0,0 +1,44 @@
+// LeituraSupervisorio.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+std::vector<std::string> SepararString(std::string mensagem, std::string delimitador) {
+    std::vector<std::string> resultado;
+
+    unsigned inicio = 0U;
+    auto fim = mensagem.find(delimitador);
+
+    while (fim != std::string::npos) {
+        std::cout << mensagem.substr(inicio, fim - inicio) << std::endl;
+        resultado.push_back(mensagem.substr(inicio, fim - inicio));
+        inicio = fim + delimitador.length();
+        fim = mensagem.find(delimitador, inicio);
+    }
+
+
+
+    return resultado;
+}
+
+
+
+int main()
+{
+    std::string mensagem = "1#Teste#000030#123123#vinte";
+    SepararString(mensagem, "#");
+}
+
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
