5fe5af810f91de51f99decaa97c4b2ead6ca7df8
Thiagorss54 <thiagorss54@gmail.com>
2022-01-11 14:32:48 -0300

    [Thiago] Adicionando função LerMensagem no LeituraSupervisorio

--- LeituraSupervisorio/LeituraSupervisorio.h
+++ LeituraSupervisorio/LeituraSupervisorio.h
@@ -1,15 +1,32 @@
 #pragma once
-
-
+#include <string>
+struct Mensagem
+{
+	int tipo;
+	int nseq;
+	float tzona1;
+	float tzona2;
+	float tzona3;
+	float volume;
+	float pressao;
+	char  timestamp[13];
+	
+};
 class LeituraSupervisorio {
 private:
 	bool status;
+	int nseq;
 
 public:
 	LeituraSupervisorio();
 	void AlterarStatus();
 	bool GetStatus();
-
+	void LerMensagem(); //simula a leitura através da geração de dados	
+	std::string GetHorario();
+	std::string Formatar(int valor,const int tamanho);
+	std::string Formatar(float valor, const int tamanho);
+	float ValorAleatorio(float max);
+	
 };
 
 
