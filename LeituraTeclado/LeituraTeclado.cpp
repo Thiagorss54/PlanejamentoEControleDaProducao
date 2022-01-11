54d5b06b686df6a1201f94b0880bb3b0789292fe
Gabriel Alves <gabriel.alves@dtidigital.com.br>
2022-01-11 20:24:06 -0300

    [Gabriel] Ajustes na interface do teclado

--- LeituraTeclado/LeituraTeclado.cpp
+++ LeituraTeclado/LeituraTeclado.cpp
@@ -8,45 +8,35 @@
 
 using namespace std;
 
-
-
-
 void ExecutarInstrucao(char instrucao, LeituraSupervisorio *leituraSupervisorio, bool& p, bool& r, bool& s, bool& e)
 {
 	switch (tolower(instrucao)) {
 	case ('l'):
 		leituraSupervisorio->AlterarStatus();
-		
 		break;
 
 	case ('p'):
-		//LeituraPCP.AlterarStatus();
-		p = !p;
+		//leituraPCP->AlterarStatus();
 		break;
 
 	case ('r'):
-		//CapturaMensagens.AlterarStatus();
-		r = !r;
+		//capturaMensagens->AlterarStatus();
 		break;
 
 	case ('s'):
-		//DadosProcesso.AlterarStatus();
-		s = !s;
+		//dadosProcesso->AlterarStatus();
 		break;
 
 	case ('e'):
-		//GestaoProducao.AlterarStatus();
-		e = !e;
+		//gestaoProducao->AlterarStatus();
 		break;
 
 	case ('1'):
-		//DadosProcesso.LimparConsole();
-		cout << "1";
+		//dadosProcesso->LimparConsole();
 		break;
 
 	case ('2'):
-		//GestaoProducao.LimparConsole();
-		cout << "2";
+		//gestaoProducao->LimparConsole();
 		break;
 
 	case (27):
@@ -61,12 +51,12 @@ void ExecutarInstrucao(char instrucao, LeituraSupervisorio *leituraSupervisorio,
 void GerarDashboard(LeituraSupervisorio *leituraSupervisorio, bool p, bool r, bool s, bool e)
 {
 	cout << "Instrucao 'l' - Leitura do Supervisorio \tStatus: " << leituraSupervisorio->GetStatus() << " \n";
-	cout << "Instrucao 'p' - Leitura do PCP \tStatus: " << p << " \n";
-	cout << "Instrucao 'r' - CapturaMensagens \tStatus: " << r << " \n";
-	cout << "Instrucao 's' - DadosProcesso \tStatus: " << s << " \n";
-	cout << "Instrucao 'e' - GestaoProducao \tStatus: " << e << " \n";
-	cout << "Instrucao '1' - Limpar console DadosProcesso\n";
-	cout << "Instrucao '2' - Limpar console GestaoProducao\n";
+	cout << "Instrucao 'p' - Leitura do PCP \tStatus: " << p << " \n"; //leituraPCP->GetStatus()
+	cout << "Instrucao 'r' - CapturaMensagens \tStatus: " << r << " \n"; //capturaMensagens->GetStatus()
+	cout << "Instrucao 's' - DadosProcesso \tStatus: " << s << " \n"; //dadosProcesso->GetStatus()
+	cout << "Instrucao 'e' - GestaoProducao \tStatus: " << e << " \n"; //gestaoProducao->GetStatus()
+	cout << "Instrucao '1' - Limpar console DadosProcesso\n"; //dadosProcesso->GetStatus()
+	cout << "Instrucao '2' - Limpar console GestaoProducao\n"; //gestaoProducao->GetStatus()
 	cout << "Instrucao 'esc' - Encerrar processos\n";
 }
 
